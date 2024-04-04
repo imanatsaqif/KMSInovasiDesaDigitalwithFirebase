2fa3085fd14f3af477e733037e28ff5837e0b738
Imana Tsaqif <iimtsaqif@apps.ipb.ac.id>
2024-04-04 10:49:37 +0700

    add authentication  and store user data

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,11 +1,42 @@
-import api from "./api";
+import { ath, db } from "../firebase/clientApp";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
 
-type Login = {
+type LoginData = {
   email: string;
   password: string;
 };
 
-export const login = async ({ email, password }: Login): Promise<any> =>
-  await api.get("/users", { params: { email, password } });
-export const register = async (body: any): Promise<any> =>
-  await api.post("/users", body);
+type RegisterData = {
+  email: string;
+  password: string;
+  role: string;
+};
+
+//Login dengan Firebase
+export const login = async ({ email, password }: LoginData): Promise<any> => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(ath, email, password);
+    return userCredential.user;
+  } catch (error: any) {
+    throw new Error(error.message || "An error occurred during authentication");
+  }  
+};
+
+//Register dengan Firebase dan menyimpannya dii Firestore
+export const register = async ({ email, password, role }: RegisterData): Promise<any> => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(ath, email, password);
+    const user = userCredential.user;
+
+    // Menyimpan data pengguna ke Firestore
+    await setDoc(doc(db, "users", user.uid), {
+      email: user.email,
+      role: role
+    });
+
+    return user;
+  } catch (error: any) {
+    throw new Error(error.message || "An error occurred during authentication");
+  }
+};
