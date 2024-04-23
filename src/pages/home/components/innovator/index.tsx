38c8cc48ed9f05feb08a83502c0eaa196d57500c
Imana Tsaqif <iimtsaqif@apps.ipb.ac.id>
2024-04-23 17:26:52 +0700

    Modifying 'Innovator Unggulan' Card

--- src/pages/home/components/innovator/index.tsx
+++ src/pages/home/components/innovator/index.tsx
@@ -1,31 +1,28 @@
 import { paths } from "Consts/path";
 import { useQuery } from "react-query";
 import Container from "Components/container";
-import { getUsers } from "Services/userServices";
+import { getInnovators } from "Services/userServices"; //Memanggil get Innovator dari UserServices
 import CardInovator from "Components/card/innovator";
 import { generatePath, useNavigate } from "react-router-dom";
 import { CardContainer, Title, Horizontal } from "./_innovatorStyle";
 
 function Innovator() {
   const navigate = useNavigate();
-  const { data: users } = useQuery<any>("completedInnovatorsProfile", getUsers);
-  const innovators = users?.filter((item: any) => item.role === "innovator");
-  const completedProfile = innovators?.filter((item: any) =>
-    Object.hasOwn(item, "innovatorName")
-  );
+
+  const { data: innovators, isFetched } = useQuery("innovators", getInnovators);
 
   return (
     <Container>
       <Title>Inovator Unggulan</Title>
       <CardContainer>
         <Horizontal>
-          {completedProfile?.map((item: any, idx: number) => (
+          {innovators && innovators.map((innovator) => (
             <CardInovator
-              key={idx}
-              {...item}
+              key={innovator.id}
+              {...innovator}
               onClick={() =>
                 navigate(
-                  generatePath(paths.DETAIL_INNOVATOR_PAGE, { id: item?.id })
+                  generatePath(paths.DETAIL_INNOVATOR_PAGE, { id: innovator.id })
                 )
               }
             />
