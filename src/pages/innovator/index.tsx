14b76a6c4e72f9a5427820cf42189ecf1c01441a
Imana Tsaqif <iimtsaqif@apps.ipb.ac.id>
2024-04-23 17:21:45 +0700

    Switching user methods to Firestore queries

--- src/pages/innovator/index.tsx
+++ src/pages/innovator/index.tsx
@@ -3,21 +3,20 @@ import Hero from "./components/hero";
 import CardInnovator from "Components/card/innovator";
 import Container from "Components/container";
 import { useQuery } from "react-query";
-import { getUsers } from "Services/userServices";
+import { getInnovators } from "Services/userServices"; //Memanggil get Innovator dari UserServices
 import { paths } from "Consts/path";
 import { useNavigate, generatePath } from "react-router-dom";
 import { GridContainer } from "./_innovatorStyle";
 
+
 function Innovator() {
   const navigate = useNavigate();
-  const { data: users, isFetched } = useQuery<any>("innovators", getUsers);
-  const innovators = users?.filter((item: any) => item.role === "innovator");
+  const { data: innovators, isFetched } = useQuery("innovators", getInnovators);
 
   return (
     <Container page>
       <TopBar title="Inovator" />
       <Hero />
-
       <GridContainer>
         {isFetched &&
           innovators?.map((item: any, idx: number) => (
