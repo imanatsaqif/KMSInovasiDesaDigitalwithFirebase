f8c5244285a2af5405a7c7383b3d99bff372a602
Imana Tsaqif <iimtsaqif@apps.ipb.ac.id>
2024-06-17 10:37:37 +0700

    Package, vite.config, and db Changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as url from 'url'
 import path from 'path'
+import { readFileSync } from 'fs';
 
 const dirName = url.fileURLToPath(new URL('.', import.meta.url))
+
+const env = Object.fromEntries(
+  readFileSync(path.join(dirName, '.env.local'), 'utf-8')
+    .split('\n')
+    .filter(Boolean)
+    .map(line => line.split('='))
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +24,13 @@ export default defineConfig({
       Services: path.resolve(dirName, 'src/services'),
       Hooks: path.resolve(dirName, 'src/hooks'),
     },
+  },
+  define: {
+    'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(env.VITE_PUBLIC_FIREBASE_API_KEY),
+    'process.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(env.VITE_FIREBASE_AUTH_DOMAIN),
+    'process.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(env.VITE_FIREBASE_PROJECT_ID),
+    'process.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify(env.VITE_FIREBASE_STORAGE_BUCKET),
+    'process.env.VITE_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(env.VITE_FIREBASE_MESSAGING_SENDER_ID),
+    'process.env.VITE_FIREBASE_APP_ID': JSON.stringify(env.VITE_FIREBASE_APP_ID),
   },
 })
