a1990906fdac48d4e3986c6484f3c0d945a65461
Imana Tsaqif <iimtsaqif@gmail.com>
2025-08-04 23:18:30 +0700

    Fix read method in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as url from 'url'
 import path from 'path'
-import { readFileSync } from 'fs';
 
 const dirName = url.fileURLToPath(new URL('.', import.meta.url))
 
-const env = Object.fromEntries(
-  readFileSync(path.join(dirName, '.env.local'), 'utf-8')
-    .split('\n')
-    .filter(Boolean)
-    .map(line => line.split('='))
-);
+export default defineConfig(({ mode }) => {
+  // Ambil env dari Vite (termasuk dari Vercel jika diset di dashboard)
+  const env = loadEnv(mode, process.cwd(), '')
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      Assets: path.resolve(dirName, 'src/assets'),
-      Components: path.resolve(dirName, 'src/components'),
-      Consts: path.resolve(dirName, 'src/consts'),
-      Services: path.resolve(dirName, 'src/services'),
-      Hooks: path.resolve(dirName, 'src/hooks'),
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        Assets: path.resolve(dirName, 'src/assets'),
+        Components: path.resolve(dirName, 'src/components'),
+        Consts: path.resolve(dirName, 'src/consts'),
+        Services: path.resolve(dirName, 'src/services'),
+        Hooks: path.resolve(dirName, 'src/hooks'),
+      },
     },
-  },
-  define: {
-    'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(env.VITE_PUBLIC_FIREBASE_API_KEY),
-    'process.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(env.VITE_FIREBASE_AUTH_DOMAIN),
-    'process.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(env.VITE_FIREBASE_PROJECT_ID),
-    'process.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify(env.VITE_FIREBASE_STORAGE_BUCKET),
-    'process.env.VITE_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(env.VITE_FIREBASE_MESSAGING_SENDER_ID),
-    'process.env.VITE_FIREBASE_APP_ID': JSON.stringify(env.VITE_FIREBASE_APP_ID),
-  },
+    define: {
+      'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(env.VITE_FIREBASE_API_KEY),
+      'process.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(env.VITE_FIREBASE_AUTH_DOMAIN),
+      'process.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(env.VITE_FIREBASE_PROJECT_ID),
+      'process.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify(env.VITE_FIREBASE_STORAGE_BUCKET),
+      'process.env.VITE_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(env.VITE_FIREBASE_MESSAGING_SENDER_ID),
+      'process.env.VITE_FIREBASE_APP_ID': JSON.stringify(env.VITE_FIREBASE_APP_ID),
+    },
+  }
 })
